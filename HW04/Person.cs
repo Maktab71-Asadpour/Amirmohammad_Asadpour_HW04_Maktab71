c55da48181312fb66cb0f8b28700a7ad521a2bc9
amir asadpour <amirmohammad.asadpoor@gmail.com>
2022-03-28 01:14:21 +0430

    Add project files.

---
+++ HW04/Person.cs
@@ -0,0 +1,28 @@
+﻿
+//------------- Tamrin 1 -------------
+
+public class Person
+{
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+    public string? FatherName { get; set; }
+    public string? NationalCode { get; private set; }
+    public string? PhoneNumber { get; set; }
+    public int[] Birthday { get; set; }
+    public int Height { get; set; }
+    public int Weight { get; set; }
+
+
+    public Person(string? firstName, string? lastName, string? fatherName, string? nationalCode, string? phoneNum,
+        int[] birthday, int height, int weight)
+    {
+        FirstName = firstName;
+        LastName = lastName;
+        FatherName = fatherName;
+        PhoneNumber = phoneNum;
+        Birthday = birthday;
+        Height = height;
+        Weight = weight;
+        NationalCode = nationalCode;
+    }
+}
---
+++ HW04/Program.cs
@@ -0,0 +1,55 @@
+﻿
+List<Person> personList = new List<Person>();
+bool canContinue = true;
+
+while (canContinue)
+{
+    //---------------- Tamrin 1.1 ----------------
+    Console.Write("Enter your first name: ");
+    string firstName = Console.ReadLine();
+    Console.Write("Enter your last name: ");
+    string lastName = Console.ReadLine();
+    Console.Write("Enter your father name : ");
+    string fatherName = Console.ReadLine();
+    Console.Write("Enter your natonal code : ");
+    string natonalCode = Console.ReadLine();
+    Console.Write("Enter your phone number : ");
+    string phoneNumber = Console.ReadLine();
+    Console.WriteLine("Enter your birthday");
+    int[] birthday = new int[3];
+    Console.Write("Year: "); birthday[0] = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Month : "); birthday[1] = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Day : "); birthday[2] = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Enter your height : ");
+    int height = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Enter your weight : ");
+    int weight = Convert.ToInt32(Console.ReadLine());
+
+    personList.Add(new Person(firstName, lastName, fatherName, natonalCode, phoneNumber, birthday, height, weight));
+    var personListStringObj = Newtonsoft.Json.JsonConvert.SerializeObject(personList);
+    File.WriteAllText("Persons.txt", personListStringObj);
+    Console.WriteLine("\n");
+
+
+
+    //---------------- Tamrin 1.2 ----------------
+    List<Person>? fileQueryPersonList = Newtonsoft.Json.JsonConvert.DeserializeObject<List<Person>>(File.ReadAllText("Persons.txt"));
+    foreach (var personItem in fileQueryPersonList)
+    {
+        Console.WriteLine($"Name is {personItem.FirstName + " " + personItem.LastName} and National Code is {personItem.NationalCode}");
+    }
+    Console.WriteLine("\n");
+
+
+
+    //---------------- Tamrin 1.3 ----------------
+    Console.Write("Do you want to exit ? (y = yes)  ");
+    if (Console.ReadKey().Key == ConsoleKey.Y)
+    {
+        File.WriteAllText("Persons.txt", "");
+        canContinue = false;
+    }
+    Console.WriteLine("\n");
+
+}
+
